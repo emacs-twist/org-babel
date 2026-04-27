46394a90361ff057a655145dc7cef875a23ba1af
Akira Komamura <akira.komamura@gmail.com>
2026-04-28 02:02:46 +0900

    style: Apply nixfmt

--- nix/tangleOrgBabel.nix
+++ nix/tangleOrgBabel.nix
@@ -1,7 +1,11 @@
 # Quick-and-dirty re-implementation of org-babel-tangle in Nix.
-{ languages ? [ "emacs-lisp" "elisp" ]
-, processLines ? lines: lines
-, tangleArg ? "yes"
+{
+  languages ? [
+    "emacs-lisp"
+    "elisp"
+  ],
+  processLines ? lines: lines,
+  tangleArg ? "yes",
 }:
 string:
 with builtins;
@@ -12,26 +16,23 @@ let
 
   blockStartRegexp =
     "[[:space:]]*\#\\+[Bb][Ee][Gg][Ii][Nn]_[Ss][Rr][Cc][[:space:]]+"
-    + "(" + (concatStringsSep "|" languages) + ")"
+    + "("
+    + (concatStringsSep "|" languages)
+    + ")"
     + "(([[:space:]].*)?)";
 
   parseParamsString = import ./parseParamsString.nix;
 
-  parseParamsString' = s:
-    if s == null
-    then { }
-    else parseParamsString s;
+  parseParamsString' = s: if s == null then { } else parseParamsString s;
 
-  checkBlockParams = attrs:
-    foldl' (acc: value: acc && value) true
-      (attrValues
-        (mapAttrs (name: value:
-          if name == ":tangle"
-          then value == tangleArg
-          else true
-        ) attrs));
+  checkBlockParams =
+    attrs:
+    foldl' (acc: value: acc && value) true (
+      attrValues (mapAttrs (name: value: if name == ":tangle" then value == tangleArg else true) attrs)
+    );
 
-  isBlockStart = line:
+  isBlockStart =
+    line:
     (match blockStartRegexp line != null)
     && checkBlockParams (parseParamsString' (elemAt (match blockStartRegexp line) 2));
 
@@ -41,16 +42,18 @@ let
 
   isBlockEnd = line: match blockEndRegexp line != null;
 
-  go = acc: xs:
+  go =
+    acc: xs:
     let
       st1 = dropUntil isBlockStart xs;
       st2 = splitListWith isBlockEnd st1;
     in
-    if length xs == 0
-    then acc
-    else if length st1 == 0
-    then acc
-    else (go (acc ++ [ st2.before ]) st2.after);
+    if length xs == 0 then
+      acc
+    else if length st1 == 0 then
+      acc
+    else
+      (go (acc ++ [ st2.before ]) st2.after);
 
 in
 concatStringsSep "\n" (concatLists (go [ ] (processLines lines)))
